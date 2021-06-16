67bcf1d140dc7e33f934e5c2ce919f3fe9571d35
Waylon Dixon <wayjay4@yahoo.com>
2021-06-16 11:57:34 -0700

    enabled api, added api route to get weather data from database

--- app/Http/Controllers/Weatherforcast/WeatherforcasterController.php
+++ app/Http/Controllers/Weatherforcast/WeatherforcasterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Weatherforcast;
 
 use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
+use App\Models\Weatherforcast;
 
 class WeatherforcasterController extends ApiController
 {
@@ -14,72 +15,8 @@ class WeatherforcasterController extends ApiController
      */
     public function index()
     {
-        //
-    }
+        $weather = Weatherforcast::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return $this->showAll($weather);
     }
 }
